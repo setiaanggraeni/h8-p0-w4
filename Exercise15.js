56df235e2208777c3de10281292f8b1d6ae52044
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-04-05 13:32:36 +0700

    Password Generator

--- Exercise15.js
+++ Exercise15.js
@@ -1,28 +1,92 @@
 function changeVocals (str) {
-    return str.replace(/[a-zA-Z]/g, function(i){
-        return String.fromCharCode(i.charCodeAt(0)+1);
-    });
-}
+    var result = ''
+    for (var i=0; i<str.length; i++){
+        // console.log(str[i])
+        if (str[i]==="a"){
+            result+="b"
+        }
+        else if (str[i]==="i"){
+            result+="j"
+        }
+        else if (str[i]==="u"){
+            result+="v"
+        }
+        else if (str[i]==="e"){
+            result+="f"
+        }
+        else if (str[i]==="o"){
+            result+="p"
+        }
+        else if (str[i]==="A"){
+            result+="B"
+        }
+        else if (str[i]==="I"){
+            result+="J"
+        }
+        else if (str[i]==="U"){
+            result+="V"
+        }
+        else if (str[i]==="E"){
+            result+="F"
+        }
+        else if (str[i]==="O"){
+            result+="P"
+        }
+        else {
+            result+=str[i]
+        }
 
-function reverseWord (str) {
-  return str.split("").reverse().join("");
-}
-
-function setLowerUpperCase (str) {
-  return str.replace(/([a-zA-Z])/g, function(a) {
-        return String.fromCharCode(a.charCodeAt() ^ 32);
-      });
-}
-
-function removeSpaces (str) {
-  return str.split(' ').join('');
-}
-
-function passwordGenerator (name) {
-    return name.replace(/[a-zA-Z]/g, function(i){
-          return String.fromCharCode(i.charCodeAt(0)+1);
-      });
-      str.split("").reverse().join("");
+    }
+    return result;
+  }
+  
+  function reverseWord (str) {
+    var reverse="";
+    for (var i=str.length-1; i>=0; i--){
+        reverse+=str[i];
+        // console.log(reverse)
+    }
+    return reverse;
+  }
+  
+  function setLowerUpperCase (str) {
+    var word="";
+    for (var i=0; i<str.length; i++){
+        if (str[i]===str[i].toUpperCase()){
+            word+=str[i].toLowerCase();
+        }
+        else if (str[i]===str[i].toLowerCase()){
+            word+=str[i].toUpperCase();
+        }
+    }
+    return word;
+  }
+  
+  function removeSpaces (str) {
+        var hasil = "";
+        for (var i=0; i<str.length; i++){
+            if (str[i]===" "){
+                str[i]=""
+            }
+            else {
+                hasil+=str[i]
+            }
+        }
+        return hasil;
+  }
+  
+  function passwordGenerator (name) {
+    if (name.length <5){
+        return 'Minimal karakter yang diinputkan adalah 5 karakter';
+      }
+    var ganti = changeVocals(name);
+    // return ganti;
+    var balik = reverseWord(ganti);
+    // return balik;
+    var kata = setLowerUpperCase(balik)
+    // return kata;
+    var finalResult = removeSpaces(kata);
+    return finalResult
   }
   
   console.log(passwordGenerator('Sergei Dragunov')); // 'VPNVGBRdJFGRFs'
