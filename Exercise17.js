8b1edc4cf8a1e118f112e616e033f4ecfd510fab
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-04-12 15:32:42 +0700

    otal Digit Rekursif

--- Exercise17.js
+++ Exercise17.js
@@ -1,19 +1,16 @@
 function totalDigitRekursif(angka) {
-    var nilai=String(angka);
-    var jumlah=0;
-      for (var i=0; i<nilai.length; i++){
-        var x= Number(nilai[i]);
-        jumlah += x;
-      }
-   return jumlah;   
+  angka=angka.toString()
+  if (angka.length===0){
+    return 0
   }
-  
-  
-  // TEST CASES
-  console.log(totalDigitRekursif(512)); // 8
-  console.log(totalDigitRekursif(1542)); // 12
-  console.log(totalDigitRekursif(5)); // 5
-  console.log(totalDigitRekursif(21)); // 3
-  console.log(totalDigitRekursif(11111)); // 5
-  
-  
+    else {
+      return Number(angka[0])+totalDigitRekursif(angka.slice(1))
+    }
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
